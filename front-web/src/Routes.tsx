bb1fdce41f58b76f0761442bc99d6d31c757c495
Fabricio Castro <baptistafabricio87@gmail.com>
2020-09-18 03:11:02 -0300

    Created records list page

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from "./pages/home";
-import Header from "./components/Header";
+import Header from "./components/header";
 import Records from "./pages/records";
 
 const Routes = () => (
